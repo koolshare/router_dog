b2665e64c132a2273f93c1bea50e966b66458f41
carseason <757841203@qq.com>
2021-02-17 00:08:47 +0800

    update 1.2

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "路由狗，路由看家好帮手~", 
     "home_url": "Module_routerdog.asp", 
-    "md5": "5efb05560c2ddfe8319fd4a1b39c0ad1", 
+    "md5": "ad501f64e5bb18274cd4a2a435f59272", 
     "module": "routerdog", 
     "title": "路由狗", 
-    "version": "1.1.1"
+    "version": "1.2"
 }
